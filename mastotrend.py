88d1bbd9464ac4b88befc92c96ad3978faabefd3
David Ron <dron@cj.com>
2018-01-14 20:04:59 -0800

    Filtering out common words, other NLP stuff.

--- mastotrend.py
+++ mastotrend.py
@@ -6,9 +6,11 @@
 import json
 from urllib.request import urlopen
 import nltk  
-from nltk.tokenize import TweetTokenizer 
+from nltk.tokenize import TweetTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer 
 from bs4 import BeautifulSoup
 import string
+import contractions
 from nltk.corpus import stopwords
 from functools import reduce
 # pipreqs --force .
@@ -16,10 +18,14 @@ from functools import reduce
 
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('wordnet')
 
 #timelineUrl = "https://mastodon.sdf.org/api/v1/timelines/public";
 
 #data = json.load(urlopen(timelineUrl))
+
+top_1000_words = set(line.strip() for line in open('topwords.txt'))
+top_1000_words.update(["cannot"])
 
 
 
@@ -40,16 +46,25 @@ data = getLotsOfToots()
 
 
 def words(text):
+    def word_fix(word):
+        word = word.lower()
+        word = lemmatizer.lemmatize(word)
+        return word
+        
+    lemmatizer = WordNetLemmatizer()
     tokenized = TweetTokenizer().tokenize(text)
+    lemmatized = map(word_fix , tokenized)
+    filtered = filter(lambda word: not word in top_1000_words, lemmatized)
+    
     #print(tokenized)
-    all_words = set(TweetTokenizer().tokenize(text)) #the set prevents a single status from boosting a word multiple times.
+    all_words = set(filtered) #the set prevents a single status from boosting a word multiple times.
     words_without_stopwords = filter(lambda w: len(w) > 4 and not w in set(stopwords.words('english')), all_words)
     words = words_without_stopwords
     
     return list(words)
 
 def html_to_string (html):
-    return words(BeautifulSoup(html, "html.parser").get_text())
+    return words(contractions.fix(BeautifulSoup(html, "html.parser").get_text()))
 
 big_list_of_words = reduce((lambda x,y: x+y), map(lambda status: html_to_string(status["content"]), data))
 frequency_dist = nltk.probability.FreqDist(big_list_of_words)
